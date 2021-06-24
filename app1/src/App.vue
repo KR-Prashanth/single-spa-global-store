1ac813a8be20fcecdbfbe212c81a994901997349
Prashanth-Primaseller <prasanth@primaseller.com>
2021-06-24 17:01:31 +0530

    adding back apps

---
+++ app1/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <img
+      alt="Vue logo"
+      width="70px"
+      src="./assets/logo.png"
+      @click="incrementCounter"
+    />
+    <div>APP 1: {{ count }}</div>
+  </div>
+</template>
+
+<script>
+import { counter, incrementCounter } from "@demo/store";
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+     // count: counter,
+    };
+  },
+
+  computed: {
+    count() {
+      return counter;
+    }
+  },
+  mounted() {
+    console.log(counter.value);
+  },
+  methods: {
+    incrementCounter() {
+      incrementCounter();
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
